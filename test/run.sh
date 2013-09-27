62bcfdd6477182dccdacd0703a849d625ca520f4
Alexei Sholik <alcosholik@gmail.com>
2013-09-27 17:05:50 +0300

    Clean up the tests

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-./line_tokenizer_test &&
-lua file_tokenizer_test.lua &&
-lua parser_test.lua &&
-lua file_parser_test.lua &&
-lua typecheck_test.lua
+parent_dir=$(cd ..; pwd)
+export LUA_PATH="$parent_dir/?.lua;;"
+
+./line_tokenizer_test          \
+&& lua file_tokenizer_test.lua \
+&& lua parser_test.lua         \
+&& lua file_parser_test.lua    \
+#&& lua typecheck_test.lua      \
